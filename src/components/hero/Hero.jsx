624a2c0c632fa46b6f4e2c439d895ca5c9ccdf06
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-05-20 23:01:43 +0200

    add and style hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,16 @@
 import './Hero.scss';
+import heroGrid from '../../assets/images/hero_grid.png';
 
 export default function Hero() {
     return (
-        <div className='hero'></div>
+        <div className='hero'>
+            <div className='hero__grid-container'>
+                <img class="hero__grid-image" src={heroGrid} alt="A grid of pictures"></img>
+                <h2 class="hero__h2">Online Experiences</h2>
+                <p class="hero__p">
+                    Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+                </p>
+            </div>
+        </div>
     )
 } 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import './Navbar.scss';
-import airbnblogo from '../../airbnb_logo.png';
+import airbnblogo from '../../assets/logo/airbnb_logo.png';
 
 export default function Navbar() {
     return (
         <nav className='navbar'>
-            <img src={airbnblogo}></img>
+            <img src={airbnblogo} alt="Airbnb logo along with the brand name"></img>
         </nav>
     )
 } 
