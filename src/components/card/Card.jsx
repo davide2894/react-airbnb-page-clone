0d7f46af3f1db617c760a199853ddd5fa431fb4c
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-05-23 08:21:15 +0200

    finish style

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -12,15 +12,18 @@ export default function Card () {
                 <img className='card__image' src={swimAthlete} alt='A smiling swim athlete' />
             </div>
             <div className='card__rating-container'>
-                <img className='card__rating-star' src={starIcon} aria-hidden='true' />
-                <span>5.0 (6)</span>
+                <img className='card__rating-star card__rating-child' src={starIcon} aria-hidden='true' />
+                <span className='card__rating-child'>5.0</span>
+                <span className='card__gray-colored card__rating-child'>(6)</span>
+                <span className='card__gray-colored card__dot card__rating-child'></span>
+                <span className='card__gray-colored card__rating-child'>USA</span>
             </div>
             <div className='card__description'>
                 <span>Life lessons with Katie Zaferes</span>
             </div>
             <div className='card__price-container'>
                 <span className='card__price'>From $136</span>
-                <span className='card__price-person'>/ person</span>
+                <span className='card__price-person'> / person</span>
             </div>
         </div>
     );
