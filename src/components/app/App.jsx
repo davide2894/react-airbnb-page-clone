3de042f15c5e7c803edce6e3667afa727acbfdf2
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-05-24 13:45:38 +0200

    add props to card component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,13 +2,33 @@ import './App.scss';
 import Navbar from '../navbar/Navbar';
 import Hero from '../hero/Hero';
 import Card from '../card/Card';
+import swimAthlete from '../../assets/images/swim_athlete.png';
+
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
 
 export default function App() {
   return (
     <div className="app">
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <Card></Card>
+      <Navbar/>
+      <Hero/>
+      <Card
+        img={swimAthlete}
+        rating="5.0"
+        reviewCount=" (6)"
+        country="US"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,28 +1,27 @@
 import './Card.scss';
-import swimAthlete from '../../assets/images/swim_athlete.png';
 import starIcon from '../../assets/icons/star_icon.png';
 
-export default function Card () {
+export default function Card (props) {
     return (
         <div className='card'>
             <div className='card__image-container'>
                 <span className='card__badge'>
                     SOLD OUT
                 </span>
-                <img className='card__image' src={swimAthlete} alt='A smiling swim athlete' />
+                <img className='card__image' src={props.img} alt='A smiling swim athlete' />
             </div>
             <div className='card__rating-container'>
                 <img className='card__rating-star card__rating-child' src={starIcon} aria-hidden='true' />
-                <span className='card__rating-child'>5.0</span>
-                <span className='card__gray-colored card__rating-child'>(6)</span>
+                <span className='card__rating-child'>{props.rating}</span>
+                <span className='card__gray-colored card__rating-child'>{props.reviewCount}</span>
                 <span className='card__gray-colored card__dot card__rating-child'></span>
-                <span className='card__gray-colored card__rating-child'>USA</span>
+                <span className='card__gray-colored card__rating-child'>{props.country}</span>
             </div>
             <div className='card__description'>
-                <span>Life lessons with Katie Zaferes</span>
+                <span>{props.title}</span>
             </div>
             <div className='card__price-container'>
-                <span className='card__price'>From $136</span>
+                <span className='card__price'>From ${props.price}</span>
                 <span className='card__price-person'> / person</span>
             </div>
         </div>
