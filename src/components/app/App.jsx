d74a73e6520e64f00dd0b679c2def222ddf5b0a1
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-05-25 20:23:33 +0200

    use props with unique object

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -21,13 +21,7 @@ export default function App() {
   const cards = cardsData.map(cardData => {
     return <Card
       key={cardData.id.toString()} 
-      img={cardData.coverImg}
-      rating={cardData.stats.rating}
-      reviewCount={cardData.stats.reviewCount}
-      location={cardData.location}
-      title={cardData.title}
-      price={cardData.price}
-      openSpots={cardData.openSpots}
+      cardData={cardData}
     />  
   });
 
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,8 +2,9 @@ import './Card.scss';
 import starIcon from '../../assets/icons/star_icon.png';
 
 export default function Card (props) {
-    let isSoldout = props.openSpots === 0;
-    let isOnline = props.location == "Online";
+
+    let isSoldout = props.cardData.openSpots === 0;
+    let isOnline = props.cardData.location == "Online";
     let badgeText;
     
     if(isSoldout) {
@@ -16,20 +17,20 @@ export default function Card (props) {
         <div className='card'>
             <div className='card__image-container'>
                 {badgeText && <span className="card__badge">{badgeText}</span>}
-                <img className='card__image' src={require(`../../assets/images/${props.img}`)} alt='A smiling swim athlete' />
+                <img className='card__image' src={require(`../../assets/images/${props.cardData.coverImg}`)} alt='A smiling swim athlete' />
             </div>
             <div className='card__rating-container'>
                 <img className='card__rating-star card__rating-child' src={starIcon} aria-hidden='true' />
-                <span className='card__rating-child'>{props.rating}</span>
-                <span className='card__gray-colored card__rating-child'>{props.reviewCount}</span>
+                <span className='card__rating-child'>{props.cardData.rating}</span>
+                <span className='card__gray-colored card__rating-child'>{props.cardData.reviewCount}</span>
                 <span className='card__gray-colored card__dot card__rating-child'></span>
-                <span className='card__gray-colored card__rating-child'>{props.location}</span>
+                <span className='card__gray-colored card__rating-child'>{props.cardData.location}</span>
             </div>
             <div className='card__description'>
-                <span>{props.title}</span>
+                <span>{props.cardData.title}</span>
             </div>
             <div className='card__price-container'>
-                <span className='card__price'>From ${props.price}</span>
+                <span className='card__price'>From ${props.cardData.price}</span>
                 <span className='card__price-person'> / person</span>
             </div>
         </div>
